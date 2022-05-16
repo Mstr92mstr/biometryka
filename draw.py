20c6aa6f0b6fc4eb716f9b8a3ea1f33a8f72fb50
pawel_staszewski <pawel.staszewski@gc5.pl>
2022-05-16 23:08:24 +0200

    increased readibility

--- draw.py
+++ draw.py
@@ -10,7 +10,7 @@ img = cv.imread('Resources/Photos/lady.jpg')
 blank[200:300, 300:400] = 0,255,0
 cv.imshow('Green', blank)
 
-cv.putText(blank, "Spierdalaj stara kurwo", (100,355), cv.FONT_HERSHEY_SCRIPT_COMPLEX, 1.0, (255, 255, 255), 5)
+cv.putText(blank, "Polacy nic sie nie stalo", (100,355), cv.FONT_HERSHEY_SCRIPT_COMPLEX, 1.0, (255, 255, 255), 5)
 cv.imshow('Text', blank)
 
 cv.waitKey(0)
